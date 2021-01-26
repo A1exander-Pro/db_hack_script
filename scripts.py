b0a55390417cac419aa746eb3c21150f3cb54a3d
A1exander-Pro <76167473+A1exander-Pro@users.noreply.github.com>
2021-01-26 13:16:20 +0300

    Update scripts.py

--- scripts.py
+++ scripts.py
@@ -1,20 +1,20 @@
 def fix_marks(schoolkid):
     try:
         child = Schoolkid.objects.filter(full_name__contains=schoolkid).get()
-        new_marks = Mark.objects.filter(schoolkid=child,
-                                        points__lte=3).update(points=5)
-        return new_marks
     except Schoolkid.DoesNotExist:
         raise Exception("Schoolkid does not exist")
+    new_marks = Mark.objects.filter(schoolkid=child,
+                                    points__lte=3).update(points=5)
+    return new_marks
 
 
 def remove_chastisements(schoolkid):
     try:
         child = Schoolkid.objects.filter(full_name__contains=schoolkid).get()
-        deleted_chastisement = Chastisement.objects.filter(schoolkid=child).delete()
-        return deleted_chastisement
     except Schoolkid.DoesNotExist:
         raise Exception("Schoolkid does not exist")
+    deleted_chastisement = Chastisement.objects.filter(schoolkid=child).delete()
+    return deleted_chastisement
 
 
 def create_commendation(schoolkid, lesson):
@@ -26,29 +26,29 @@ def create_commendation(schoolkid, lesson):
 
     try:
         child = Schoolkid.objects.filter(full_name__contains=schoolkid).get()
-        child_lessons = Lesson.objects.filter(year_of_study=child.year_of_study,
-                                              group_letter=child.group_letter,
-                                              subject__title=lesson)
-        child_lesson = child_lessons.order_by("-date").first()
-        if child_lesson is None:
-            return "Такого урока не существует"
-        lesson_date = child_lesson.date
-        lesson_teacher = child_lesson.teacher
-        commendation = Commendation.objects.filter(created=lesson_date,
-                                                   schoolkid=child,
-                                                   subject__title=lesson,
-                                                   teacher=lesson_teacher).first()
-        if commendation is None:
-            new_commendation = Commendation.objects.create(text=random.choice(commendation_text),
-                                                           created=lesson_date,
-                                                           schoolkid=child,
-                                                           subject=child_lesson.subject,
-                                                           teacher=lesson_teacher)
-            return new_commendation
-        else:
-            return "В этом уроке уже есть похвала, попробуй другой"
     except Schoolkid.DoesNotExist:
         raise Exception("Schoolkid does not exist")
+    child_lessons = Lesson.objects.filter(year_of_study=child.year_of_study,
+                                          group_letter=child.group_letter,
+                                          subject__title=lesson)
+    child_lesson = child_lessons.order_by("-date").first()
+    if child_lesson is None:
+        return "Такого урока не существует"
+    lesson_date = child_lesson.date
+    lesson_teacher = child_lesson.teacher
+    commendation = Commendation.objects.filter(created=lesson_date,
+                                               schoolkid=child,
+                                               subject__title=lesson,
+                                               teacher=lesson_teacher).first()
+    if commendation is None:
+        new_commendation = Commendation.objects.create(text=random.choice(commendation_text),
+                                                       created=lesson_date,
+                                                       schoolkid=child,
+                                                       subject=child_lesson.subject,
+                                                       teacher=lesson_teacher)
+        return new_commendation
+    else:
+        return "В этом уроке уже есть похвала, попробуй другой"
 
 
 
